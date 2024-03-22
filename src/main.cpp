fd17f0294ce9641829753791eb16b90b8bb6ce5a
afshirazi <ashirazi@purdue.edu>
2024-03-21 22:11:43 -0400

    made names more consistent, added comments, refactored code to remove some redundancy

--- src/main.cpp
+++ src/main.cpp
@@ -91,23 +91,39 @@ int main() {
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(floor_indices), floor_indices, GL_STATIC_DRAW);
 
     // VAO for particles
-    GLuint points_VAO;
-    glGenVertexArrays(1, &points_VAO);
-    glBindVertexArray(points_VAO);
+    GLuint particles_VAO;
+    glGenVertexArrays(1, &particles_VAO);
+    glBindVertexArray(particles_VAO);
 
-    GLuint points_VBO;
-    glGenBuffers(1, &points_VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, points_VBO);
+    GLuint particles_VBO;
+    glGenBuffers(1, &particles_VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, particles_VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(particles_pos), particles_pos, GL_STREAM_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
     glEnableVertexAttribArray(0);
 
     GLuint shaderProg = createAndUseShaderProgram();
 
-    glm::mat4 proj;
-    glm::mat4 model;
-    glm::mat4 view;
-    glm::mat4 transform;
+    glm::mat4 proj = glm::mat4(1.0f);
+    glm::mat4 model = glm::mat4(1.0f);
+    glm::mat4 view = glm::mat4(1.0f);
+    glm::mat4 floor_transform;
+    glm::mat4 particles_transform;
+
+    // Transformations for floor
+    proj = glm::perspective(glm::radians(55.0f), 8.f / 6.f, 0.1f, 10.0f);
+    model = glm::rotate(model, glm::radians(40.f), glm::vec3(1.f, 0.f, 0.f));
+    model = glm::rotate(model, glm::radians(-35.0f), glm::vec3(0.f, 1.f, 0.f));
+    model = glm::scale(model, glm::vec3(1.5, 1.5, 1.5));
+    view = glm::translate(view, glm::vec3(0.f, 0.2f, -1.1f));
+    floor_transform = proj * view * model;
+    
+    // Transformations for particles
+    view = glm::mat4(1.0f);
+    model = glm::scale(model, glm::vec3(0.67, 0.67, 0.67));
+    view = glm::translate(view, glm::vec3(0.f, 0.f, -1.1f));
+    particles_transform = proj * view * model;
+
     GLint transformLoc = glGetUniformLocation(shaderProg, "transform");
     GLint colorLoc = glGetUniformLocation(shaderProg, "vertColor");
 
@@ -118,46 +134,21 @@ int main() {
         glEnable(GL_BLEND);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
+        // Draw floor
         glBindVertexArray(floor_VAO);
-
-        // Transformations for floor
-        proj = glm::mat4(1.0f);
-        model = glm::mat4(1.0f);
-        view = glm::mat4(1.0f);
-        proj = glm::perspective(glm::radians(55.0f), 8.f / 6.f, 0.1f, 10.0f);
-        model = glm::rotate(model, glm::radians(40.f), glm::vec3(1.f, 0.f, 0.f));
-        model = glm::rotate(model, glm::radians(-35.0f), glm::vec3(0.f, 1.f, 0.f));
-        model = glm::scale(model, glm::vec3(1.5, 1.5, 1.5));
-        view = glm::translate(view, glm::vec3(0.f, 0.2f, -1.1f));
-
-        transform = proj * view * model;
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(floor_transform));
         glUniform3f(colorLoc, 0.5f, 0.f, 0.f); // color red
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0); // unbind
         
-        glBindVertexArray(points_VAO);
-
-        glBindBuffer(GL_ARRAY_BUFFER, points_VBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(particles_pos), particles_pos, GL_STREAM_DRAW);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
-        glEnableVertexAttribArray(0);
-        // Transformations for floor
-        //proj = glm::mat4(1.0f);
-        //model = glm::mat4(1.0f);
-        view = glm::mat4(1.0f);
-        //proj = glm::perspective(glm::radians(55.0f), 8.f / 6.f, 0.1f, 10.0f);
-        //model = glm::rotate(model, glm::radians(40.f), glm::vec3(1.f, 0.f, 0.f));
-        //model = glm::rotate(model, glm::radians(-35.0f), glm::vec3(0.f, 1.f, 0.f));
-        model = glm::scale(model, glm::vec3(0.67, 0.67, 0.67));
-        view = glm::translate(view, glm::vec3(0.f, 0.f, -1.1f));
-
-        transform = proj * view * model;
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+        // Draw particles
+        glBindVertexArray(particles_VAO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(particles_pos), particles_pos, GL_STREAM_DRAW); // Update particle positions in VBO
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(particles_transform));
         glUniform3f(colorLoc, 0.f, 0.f, 0.5f); // color blue
         glPointSize(5);
         glDrawArrays(GL_POINTS, 0, 15);
 
+        // Apply forces
         applyGrav(1.f / 10000.f);
 
         glfwSwapBuffers(window);

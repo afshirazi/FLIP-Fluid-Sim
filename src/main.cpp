65a9f07df7b1d775ed4157d8ad8b8fa4e1293ac4
afshirazi <ashirazi@purdue.edu>
2024-03-21 22:01:02 -0400

    added gravity and changed stuff around

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,46 @@
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
+void applyGrav(GLfloat dt);
+
+GLfloat particles_pos[] = {
+        -0.3f, 0.2f,  0.f,
+        -0.25f, 0.2f,  0.f,
+        -0.2f, 0.2f,  0.f,
+        -0.15f, 0.2f,  0.f,
+        -0.1f, 0.2f,  0.f,
+        -0.3f, 0.2f,  -0.1f,
+        -0.25f, 0.2f,  -0.1f,
+        -0.2f, 0.2f,  -0.1f,
+        -0.15f, 0.2f,  -0.1f,
+        -0.1f, 0.2f,  -0.1f,
+        -0.3f, 0.2f,  -0.2f,
+        -0.25f, 0.2f,  -0.2f,
+        -0.2f, 0.2f,  -0.2f,
+        -0.15f, 0.2f,  -0.2f,
+        -0.1f, 0.2f,  -0.2f,
+};
+
+GLfloat particles_vel[] = {
+        0.f, 0.f,  0.f,
+        0.f, 0.f,  0.f,
+        0.f, 0.f,  0.f,
+        0.f, 0.f,  0.f,
+        0.f, 0.f,  0.f,
+        0.f, 0.f,  0.f,
+        0.f, 0.f,  0.f,
+        0.f, 0.f,  0.f,
+        0.f, 0.f,  0.f,
+        0.f, 0.f,  0.f,
+        0.f, 0.f,  0.f,
+        0.f, 0.f,  0.f,
+        0.f, 0.f,  0.f,
+        0.f, 0.f,  0.f,
+        0.f, 0.f,  0.f,
+};
+
 int main() {
-    // Taken from learnopengl.com
+    // Some code taken from learnopengl.com
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -35,24 +73,6 @@ int main() {
         1, 3, 2
     };
 
-    GLfloat points_vertices[] = {
-        -0.3f, 0.2f,  0.f,
-        -0.25f, 0.2f,  0.f,
-        -0.2f, 0.2f,  0.f,
-        -0.15f, 0.2f,  0.f,
-        -0.1f, 0.2f,  0.f,
-        -0.3f, 0.2f,  -0.1f,
-        -0.25f, 0.2f,  -0.1f,
-        -0.2f, 0.2f,  -0.1f,
-        -0.15f, 0.2f,  -0.1f,
-        -0.1f, 0.2f,  -0.1f,
-        -0.3f, 0.2f,  -0.2f,
-        -0.25f, 0.2f,  -0.2f,
-        -0.2f, 0.2f,  -0.2f,
-        -0.15f, 0.2f,  -0.2f,
-        -0.1f, 0.2f,  -0.2f,
-    };
-
     // VAO for "floor"
     GLuint floor_VAO;
     glGenVertexArrays(1, &floor_VAO);
@@ -70,7 +90,7 @@ int main() {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, floor_EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(floor_indices), floor_indices, GL_STATIC_DRAW);
 
-    // VAO for points
+    // VAO for particles
     GLuint points_VAO;
     glGenVertexArrays(1, &points_VAO);
     glBindVertexArray(points_VAO);
@@ -78,7 +98,7 @@ int main() {
     GLuint points_VBO;
     glGenBuffers(1, &points_VBO);
     glBindBuffer(GL_ARRAY_BUFFER, points_VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(points_vertices), points_vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(particles_pos), particles_pos, GL_STREAM_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
     glEnableVertexAttribArray(0);
 
@@ -91,11 +111,12 @@ int main() {
     GLint transformLoc = glGetUniformLocation(shaderProg, "transform");
     GLint colorLoc = glGetUniformLocation(shaderProg, "vertColor");
 
+    // rendering loop
     while (!glfwWindowShouldClose(window))
     {
         glEnable(GL_DEPTH_TEST);
+        glEnable(GL_BLEND);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
         
         glBindVertexArray(floor_VAO);
 
@@ -115,24 +136,29 @@ int main() {
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0); // unbind
         
-
         glBindVertexArray(points_VAO);
 
+        glBindBuffer(GL_ARRAY_BUFFER, points_VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(particles_pos), particles_pos, GL_STREAM_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+        glEnableVertexAttribArray(0);
         // Transformations for floor
-        proj = glm::mat4(1.0f);
-        model = glm::mat4(1.0f);
+        //proj = glm::mat4(1.0f);
+        //model = glm::mat4(1.0f);
         view = glm::mat4(1.0f);
-        proj = glm::perspective(glm::radians(55.0f), 8.f / 6.f, 0.1f, 10.0f);
-        model = glm::rotate(model, glm::radians(40.f), glm::vec3(1.f, 0.f, 0.f));
-        model = glm::rotate(model, glm::radians(-35.0f), glm::vec3(0.f, 1.f, 0.f));
-        //model = glm::scale(model, glm::vec3(2, 2, 2));
+        //proj = glm::perspective(glm::radians(55.0f), 8.f / 6.f, 0.1f, 10.0f);
+        //model = glm::rotate(model, glm::radians(40.f), glm::vec3(1.f, 0.f, 0.f));
+        //model = glm::rotate(model, glm::radians(-35.0f), glm::vec3(0.f, 1.f, 0.f));
+        model = glm::scale(model, glm::vec3(0.67, 0.67, 0.67));
         view = glm::translate(view, glm::vec3(0.f, 0.f, -1.1f));
 
         transform = proj * view * model;
         glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
         glUniform3f(colorLoc, 0.f, 0.f, 0.5f); // color blue
         glPointSize(5);
-        glDrawArrays(GL_POINTS, 0, 45);
+        glDrawArrays(GL_POINTS, 0, 15);
+
+        applyGrav(1.f / 10000.f);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -144,4 +170,13 @@ int main() {
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
+}
+
+void applyGrav(GLfloat dt)
+{
+    for (int i = 1; i < 45; i += 3)
+    {
+        particles_vel[i] -= 9.82 * dt;
+        particles_pos[i] += particles_vel[i] * dt;
+    }
 }
